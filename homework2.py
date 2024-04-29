7ffa7ddb6b219329760eb35b8f99182d0b5544fc
shreyashravini <164211187+shreyashravini@users.noreply.github.com>
2024-04-28 19:02:50 -0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,126 @@
+"""
+Write your answers in the space between the questions, and commit/push only
+this file (homework2.py) and countries.csv to your repo. Note that there can 
+be a difference between giving a "minimally" right answer, 
+and a really good answer, so it can pay to put thought into your work. 
+
+This is a much longer project than those you've done in class - remember to use
+comments to help readers navigate your work!
+
+To answer these questions, you will use the two csv files provided in the repo.
+The file named gdp.csv contains the per capita GDP of many countries in and 
+around Europe in 2023 US dollars. The file named population.csv contains 
+estimates of the population of many countries.
+"""
+
+"""
+QUESTION 1
+
+Short: Open the data
+
+Long: Load the GDP data into a dataframe. Specify an absolute path using the Python 
+os library to join filenames, so that anyone who clones your homework repo 
+only needs to update one string for all loading to work.
+"""
+
+"""
+QUESTION 2
+
+Short: Clean the data
+
+Long: There are numerous issues with the data, on account of it having been 
+haphazardly assembled from an online table. To start with, the column containing
+country names has been labeled TIME. Fix this.
+
+Next, trim this down to only member states of the European Union. To do this, 
+find a list of members states (hint: there are 27 as of Apr 2024) and manually 
+create your own CSV file with this list. Name this file countries.csv. Load it 
+into a dataframe. Merge the two dataframes and keep only those rows with a 
+match.
+
+(Hint: This process should also flag the two errors in naming in gdp.csv. One 
+ country has a dated name. Another is simply misspelt. Correct these.)
+"""
+
+
+"""
+QUESTION 3
+
+Short: Reshape the data
+
+Long: Convert this wide data into long data with columns named year and gdp.
+The year column should contain int datatype objects.
+
+Remember to convert GDP from string to float. (Hint: the data uses ":" instead
+of NaN to denote missing values. You will have to fix this first.) 
+"""
+
+"""
+QUESTION 4
+
+Short: Repeat this process for the population data.
+
+Long: Load population.csv into a dataframe. Rename the TIME columns. 
+Merge it with the dataframe loaded from countries.csv. Make it long, naming
+the resulting columns year and population. Convert population and year into int.
+"""
+
+
+"""
+QUESTION 5
+
+Short: Merge the two dataframe, find the total GDP
+
+Long: Merge the two dataframes. Total GDP is per capita GDP times the 
+population.
+"""
+
+
+"""
+QUESTION 6
+
+Short: For each country, find the annual GDP growth rate in percentage points.
+Round down to 2 digits.
+
+Long: Sort the data by name, and then year. You can now use a variety of methods
+to get the gdp growth rate, and we'll suggest one here: 
+
+1. Use groupby and shift(1) to create a column containing total GDP from the
+previous year. We haven't covered shift in class, so you'll need to look
+this method up. Using groupby has the benefit of automatically generating a
+missing value for 2012; if you don't do this, you'll need to ensure that you
+replace all 2012 values with missing values.
+
+2. Use the following arithematic operation to get the growth rate:
+    gdp_growth = (total_gdp - total_gdp_previous_year) * 100 / total_gdp
+"""
+
+
+"""
+QUESTION 7
+
+Short: Which country has the highest total gdp (for the any year) in the EU? 
+
+Long: Do not hardcode your answer! You will have to put the automate putting 
+the name of the country into a string called country_name and using the following
+format string to display it:
+
+print(f"The largest country in the EU is {country_name}")
+"""
+
+
+"""
+QUESTION 8
+
+Create a dataframe that consists only of the country you found in Question 7
+
+In which year did this country have the most growth in the period 2012-23?
+
+In which year did this country have the least growth in the peroid 2012-23?
+
+Do not hardcode your answer. You will have to use the following format strings 
+to show your answer:
+
+print(f"Their best year was {best_year}")
+print(f"Their worst year was {worst_year}")
+"""
